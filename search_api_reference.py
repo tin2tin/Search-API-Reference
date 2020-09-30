2d9be2dd17bad7408eba0e3c0f41fae2835f7fdc
1C0D <63018905+1C0D@users.noreply.github.com>
2020-09-30 11:47:38 +0200

    Api 2.9 search, direct search in github & more

--- search_api_reference.py
+++ search_api_reference.py
@@ -1,16 +1,15 @@
 import bpy
 from bpy.types import Operator
 from bpy.props import (
-    EnumProperty,
+    EnumProperty,StringProperty
 )
-
 
 bl_info = {
     "name": "Search Online Reference",
     "description": "Search Online reference",
-    "author": "tintwotin",
-    "version": (0, 1),
-    "blender": (2, 80, 0),
+    "author": "tintwotin/1C0D",
+    "version": (1, 5),
+    "blender": (2, 90, 0),
     "location": "Text Editor > Edit > Search API Reference",
     "wiki_url": "https://github.com/tin2tin/Search-API-Reference",
     "tracker_url": "",
@@ -36,79 +35,59 @@ class TEXT_OT_online_reference(Operator):
              ),
              default={'API'},
         )
+        
+    s: StringProperty(default='')
 
-    @classmethod
-    def poll(cls, context):
-        return (context.space_data.text and context.area.type == 'TEXT_EDITOR')
+    def execute(self, context): 
+        
+        s=self.s        
 
-    def execute(self, context):
-        st = context.space_data
-        s = self.get_selected_text(st.text)
+        if context.area.type == 'TEXT_EDITOR' and context.space_data.text:  
+            
+            text = context.space_data.text
+            s = self.get_selected_text(text)
+            
+            if s is None:
+                bpy.ops.text.select_word()
+                s = self.get_selected_text(text)
+
+        if context.area.type == 'CONSOLE':
+            
+            s=self.s            
+            sc=context.space_data 
+            
+            if sc.select_start==sc.select_end or sc.select_start==sc.select_end+1:
+                self.report({'WARNING'}, "Selection is missing")
+
+                return {'CANCELLED'}
+            else:
+                bpy.ops.console.copy() 
+                s = bpy.context.window_manager.clipboard                    
+                
+        if context.area.type == 'INFO':
+        
+            s=self.s            
+            sc=context.space_data    
+            bpy.ops.info.report_copy()
+            s = bpy.context.window_manager.clipboard
 
         if self.type == {'API'}:
-            if s is None:
-                bpy.ops.text.select_word()
-                s = self.get_selected_text(st.text)
-                if s is not None:
-                    bpy.ops.wm.url_open(url="https://docs.blender.org/api/2.80/search.html?q="+s)
-                else:
-                    self.report({'INFO'}, "Selection is missing")
-                    return {'CANCELLED'}
-            else:
-                bpy.ops.wm.url_open(url="https://docs.blender.org/api/2.80/search.html?q="+s)
-
+            bpy.ops.wm.url_open(url="https://docs.blender.org/api/2.90/search.html?q="+s)
         if self.type == {'STACKEXCHANGE'}:
-            if s is None:
-                bpy.ops.text.select_word()
-                s = self.get_selected_text(st.text)
-                if s is not None:
-                    bpy.ops.wm.url_open(url="https://blender.stackexchange.com/search?q="+s)
-                else:
-                    self.report({'INFO'}, "Selection is missing")
-                    return {'CANCELLED'}
-            else:
-                bpy.ops.wm.url_open(url="https://blender.stackexchange.com/search?q="+s)
-
+            bpy.ops.wm.url_open(url="https://blender.stackexchange.com/search?q="+s)
         if self.type == {'PYTHON'}:
-            if s is None:
-                bpy.ops.text.select_word()
-                s = self.get_selected_text(st.text)
-                if s is not None:
-                    bpy.ops.wm.url_open(url="https://docs.python.org/3/search.html?q="+s)
-                else:
-                    self.report({'INFO'}, "Selection is missing")
-                    return {'CANCELLED'}
-            else:
-                bpy.ops.wm.url_open(url="https://docs.python.org/3/search.html?q="+s)
-
+            bpy.ops.wm.url_open(url="https://docs.python.org/3/search.html?q="+s)
         if self.type == {'SOURCECODE'}:
-            if s is None:
-                bpy.ops.text.select_word()
-                s = self.get_selected_text(st.text)
-                if s is not None:
-                    bpy.ops.wm.url_open(url="https://developer.blender.org/diffusion/B/browse/master/?grep="+s)
-                else:
-                    self.report({'INFO'}, "Selection is missing")
-                    return {'CANCELLED'}
-            else:
-                bpy.ops.wm.url_open(url="https://developer.blender.org/diffusion/B/browse/master/?grep="+s)
-
+            bpy.ops.wm.url_open(url="https://developer.blender.org/diffusion/B/browse/master/?grep="+s)
         if self.type == {'GITHUB'}:
-            if s is None:
-                bpy.ops.text.select_word()
-                s = self.get_selected_text(st.text)
-                if s is not None:
-                    bpy.ops.wm.url_open(url="https://www.google.com/search?q=intext%3A%22"+s+"%22+ext%3Apy+bpy+site%3Agithub.com")
-                else:
-                    self.report({'INFO'}, "Selection is missing")
-                    return {'CANCELLED'}
-            else:
-                bpy.ops.wm.url_open(url="https://www.google.com/search?q=intext%3A%22"+s+"%22+ext%3Apy+bpy+site%3Agithub.com")
+            # bpy.ops.wm.url_open(url="https://www.google.com/search?q=intext%3A%22"+s+"%22+ext%3Apy+bpy+site%3Agithub.com")
+            bpy.ops.wm.url_open(url="https://github.com/search?l=Python&q="+s+"&type=code")
 
         return {'FINISHED'}
 
+
     def get_selected_text(self, text):
-        """"""
+
         current_line = text.current_line
         select_end_line = text.select_end_line
 
@@ -159,20 +138,41 @@ class TEXT_OT_online_reference(Operator):
 def panel_append(self, context):
     self.layout.separator()
     self.layout.operator_menu_enum("text.online_reference", "type")
+    
+#see all actions in info    
+def update_toggle_see_all_actions(self, context):
+    
+    bpy.app.debug_wm ^= True
+
+bpy.types.Scene.toggle_see_all_actions = bpy.props.BoolProperty(
+    update=update_toggle_see_all_actions
+)    
+    
+def panel1_append(self, context):
+
+    self.layout.separator()
+    row = self.layout.row()
+    row.prop(context.scene, "toggle_see_all_actions", text="see all actions", icon='HIDE_OFF', toggle=True)
 
 
 def register():
     bpy.utils.register_class(TEXT_OT_online_reference)
     bpy.types.TEXT_MT_edit.append(panel_append)
-#   For 2.80 change _context_menu to _toolbox
     bpy.types.TEXT_MT_context_menu.append(panel_append)
+    bpy.types.CONSOLE_MT_context_menu.append(panel_append)
+    bpy.types.INFO_MT_context_menu.append(panel_append)
+    bpy.types.INFO_HT_header.append(panel1_append)
+    bpy.types.CONSOLE_MT_console.append(panel_append)
 
 
 def unregister():
     bpy.utils.unregister_class(TEXT_OT_online_reference)
     bpy.types.TEXT_MT_edit.remove(panel_append)
-    # For 2.80 change _context_menu to _toolbox
     bpy.types.TEXT_MT_context_menu.remove(panel_append)
+    bpy.types.CONSOLE_MT_context_menu.remove(panel_append)    
+    bpy.types.INFO_MT_context_menu.remove(panel_append)
+    bpy.types.INFO_HT_header.remove(panel1_append)    
+    bpy.types.CONSOLE_MT_console.remove(panel_append)
 
 
 if __name__ == "__main__":
